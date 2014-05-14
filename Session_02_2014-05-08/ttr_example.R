14b350cc284a32aa095f85d98e6d733e4de132ee
Andrei Beliankou <arbox@yandex.ru>
2014-05-15 00:46:05 +0200

    Corrected comments style for ESS.

--- Session_02_2014-05-08/ttr_example.R
+++ Session_02_2014-05-08/ttr_example.R
@@ -1,46 +1,46 @@
-# Lese die ganze Datei als eine Sammlung von Zeilen ein.
-lines <- readLines('some_file_in_TXML.xml')
+## Lese die ganze Datei als eine Sammlung von Zeilen ein.
+lines <- readLines('ablehnen.xml')
 
-# Finde die Positionsnummern von allen Tokens
-# (d.h. Zeilen, die mit "<t " beginnen).
+## Finde die Positionsnummern von allen Tokens
+## (d.h. Zeilen, die mit "<t " beginnen).
 token.indexes <- grep('<t ', lines)
 
-# Wähle alle Zeilen mit Tokens aus.
+## Wähle alle Zeilen mit Tokens aus.
 tokens <- lines[token.indexes]
 
-# Lösche alles vor dem Lemma.
+## Lösche alles vor dem Lemma.
 junk.lemmas <- sub(".+ lemma='", "", tokens)
 
-# Lösche alles nach dem Lemma.
+## Lösche alles nach dem Lemma.
 lemmas <- sub("'.+", '', junk.lemmas)
 
-# Anzahl der Types ist gleich Null.
+## Anzahl der Types ist gleich Null.
 types.count <- 0
 
-# Noch keine Types gesehen.
+## Noch keine Types gesehen.
 seen.types <- NULL
 
-# Lege eine Liste für Typesanzahl für jede Stelle im Text an.
+## Lege eine Liste für Typesanzahl für jede Stelle im Text an.
 types.on.position <- vector(length = length(lemmas))
 
-# Für jede Lemmanummer
+## Für jede Lemmanummer
 for (i in seq_along(lemmas)) {
 
-    # suche das ensprechende Lemma,
+    ## suche das ensprechende Lemma,
     lemma <- lemmas[i]
 
-    # und, falls es noch nicht gesehen wurde,
+    ## und, falls es noch nicht gesehen wurde,
     if (!(lemma %in% seen.types)) {
 
-        # zähle es als neu mit,
+        ## zähle es als neu mit,
         types.count <- types.count + 1
-        # und lege es in die Liste der gesehenen Lemmata ab.
+        ## und lege es in die Liste der gesehenen Lemmata ab.
         seen.types <- c(seen.types, lemma)
     }
 
-    # Füge die Anzahl für die aktuelle Lemmanummer in die Liste ein.
+    ## Füge die Anzahl für die aktuelle Lemmanummer in die Liste ein.
     types.on.position[i] <- types.count
 }
 
-# Zeichne eine Graphik.
+## Zeichne eine Graphik.
 plot(types.on.position, type = 'l')
